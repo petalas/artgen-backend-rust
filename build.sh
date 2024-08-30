45f4fc7e4cd2e03a6b898c96cbece5c540b6d98b
Nick Petalas <webmasternikos@gmail.com>
2024-08-30 14:36:24 +0100

    adding profiles

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # RUSTFLAGS="-O -C target-feature=+aes,+neon,+fcma"
 # RUSTFLAGS="--emit asm" cargo build --release
-RUSTFLAGS="--emit asm -C target-cpu=native" cargo build --release
+RUSTFLAGS="--emit asm -C embed-bitcode -C target-cpu=native" cargo build --release
 echo "produced: ./target/release/artgen-backend-rust"
